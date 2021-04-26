cf7135f6f1d527cc4f13e261e4ebbf0257e1c1e4
İrem Balcı <iremcibal@gmail.com>
2021-04-26 20:53:16 +0300

    method#

--- Ders2/Manager.java
+++ Ders2/Manager.java
@@ -3,10 +3,10 @@ package Ders2;
 public class Manager {
     public void joinProgram(course Course){
         System.out.println("Kursa Katılımınız Onaylanmıştır.");
-        //Method1
+        //Method 1
     }
     public void register(course Course){
         System.out.println("Kursa Kaydınız Alınmıştır.");
-        //Method2
+        //Method 2
     }
 }
